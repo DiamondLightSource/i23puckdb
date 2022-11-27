5acb6dbb8c1db17c07de86ac6939028f30ef2506
co2e14 <christian.orr@diamond.ac.uk>
2022-11-27 15:12:03 +0000

    init

---
+++ main.py
@@ -0,0 +1,45 @@
+import sqlite3
+import datetime
+
+conn = sqlite3.connect("pucks.db")
+cur = conn.cursor()
+try:
+    cur.execute("CREATE TABLE pucks(puckid type UNIQUE, person, date, location)")
+except:
+    pass
+
+def sqConnect():
+    conn = sqlite3.connect("pucks.db")
+    cur = conn.cursor()
+
+def sqDisconnect():
+    conn.close()
+
+def getInfo():
+    puckid = input("Scan puck QR or type ID: ")
+    person = input("Who are you? ")
+    today = datetime.date.today()
+    date = str(today.strftime("%d_%m_%Y"))
+    location = input("Scan in to I23 using QR or input send location: ")
+    data = (puckid, person, date, location)
+    print(data)
+    return data
+
+def sqInsert(data):
+    cur.execute("INSERT OR REPLACE INTO pucks VALUES(?, ?, ?, ?)", data)
+    conn.commit()
+
+def sqQuery():
+    for res in cur.execute("SELECT puckid, person, date, location FROM pucks"):
+        print(res)
+    conn.close()
+
+if __name__ == "__main__":
+    puckData = getInfo()
+    sqInsert(data=puckData)
+    #sqDisconnect()
+    #sqConnect()
+    sqQuery()
+
+
+
---
+++ whereIs.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect("pucks.db")
+cur = conn.cursor()
+
+findMe = input("Where is the puck ID? ")
+found = cur.execute("SELECT location FROM pucks WHERE puckid=?", (findMe,))
+found = found.fetchone()
+print(f"Puck {findMe} is at/in {found}")
