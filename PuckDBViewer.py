73ed40a21f236462c7eb38c808f7c822634c0e5c
co2e14 <christian.orr@diamond.ac.uk>
2024-08-14 12:39:08 +0100

    flask app to act as viewer

--- PuckDBViewer.py
+++ PuckDBViewer.py
@@ -4,10 +4,19 @@ import sqlite3
 
 app = Flask(__name__)
 
+def getData():
+    conn = sqlite3.connect('pucks.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT puckid, person, date, location, remark FROM pucks")
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = getData()
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
     
